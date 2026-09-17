9e16af7358f97aad6b3c42eccc88b0623b13d1e4
joaopedrofmontrezor <jp.montrezor@gmail.com>
2026-09-17 10:45:09 -0300

    feat: Base mopntada.

---
+++ calcular.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações Aritméticas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h1 align="center">Operações Aritméticas</h1>
+        <hr>
+
+        
+    </div>
+</body>
+
+</html>
