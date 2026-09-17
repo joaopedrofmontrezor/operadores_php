35afec9d3a1718cffdaecc3662eb1653a380fede
joaopedrofmontrezor <jp.montrezor@gmail.com>
2026-09-17 12:45:36 -0300

    feat: Adicionado estrutura de decisão.

--- calcular.php
+++ calcular.php
@@ -1,18 +1,23 @@
 <?php
-$n1 = (float) $_POST['n1'];
-$n2 = (float) $_POST['n2'];
+
+$n1 = isset($_POST['n1']) ? (float) $_POST['n1'] : 0;
+$n2 = isset($_POST['n2']) ? (float) $_POST['n2'] : 0;
 
 $adicao = $n1 + $n2;
 $subtracao = $n1 - $n2;
 $multiplicacao = $n1 * $n2;
-$divisao = $n1 / $n2;
-$modulo = $n1 % $n2;
+
+$divisaoValida = ($n2 != 0);
+
+
+if ($divisaoValida) {
+    $divisao = $n1 / $n2;
+    $modulo = $n1 % $n2;
+}
+
 $potencia = $n1 ** $n2;
 $concatenacao = $n1 . $n2;
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -27,9 +32,9 @@ $concatenacao = $n1 . $n2;
 <body>
     <div>
         <h1 align="center">Operações Aritméticas</h1>
-        <h2 align="center">Resultados </h1>
+        <h2 align="center">Resultados</h2>
         <hr>
-        
+
         <h3>
             <?php echo "$n1 + $n2 = $adicao"; ?>
         </h3>
@@ -37,16 +42,25 @@ $concatenacao = $n1 . $n2;
             <?php echo "$n1 - $n2 = $subtracao"; ?>
         </h3>
         <h3>
-            <?php echo "$n1 X $n2 = $multiplicacao"; ?>
+            <?php echo "$n1 × $n2 = $multiplicacao"; ?>
         </h3>
         <h3>
-            <?php echo "$n1 ÷ $n2 = ". number_format($divisao,2,',','.'); ?>
+            <?php
+                if ($divisaoValida) {
+                    echo "$n1 ÷ $n2 = " . number_format($divisao, 2, ',', '.');
+                    echo "<br>";
+                    echo "$n1 % $n2 = " . number_format($modulo, 2, ',', '.');
+                } else {
+                    echo "Não há divisão por zero.";
+                }
+            ?>
         </h3>
         <h3>
-            <?php echo "$n1 <sup> $n2 </sup> =". number_format($potencia,2,',','.'); ?>
+            <?php echo "$n1 <sup>$n2</sup> = " . number_format($potencia, 2, ',', '.'); ?>
         </h3>
-        <a href="index.php">Voltar</a>
 
+        <br>
+        <a href="index.php" class="btn-back">Voltar</a>
     </div>
 </body>
 
